9e8d68abe0b43f2dd8d14d6b1cece89101efe943
sanesco <santiagoescobar11z@gmail.com>
2019-05-22 15:51:40 -0500

    Create root POST /products and add alert to notice and add button Publicar Producto in list products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,20 @@ class ProductsController < ApplicationController
   end
 
   def new
-    @product = Product.new    
+    @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: "El producto fue publicado correctamente"
+    else
+      render :new
+    end
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :url, :description)
+    end
 end
