f74ad587213ab9833b1c7f15303accfe8d7da107
sanesco <santiagoescobar11z@gmail.com>
2019-05-23 11:40:11 -0500

    Implemented votes functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   resources :users, only: [:new, :create]
   resources :products do
+    resource :vote, only: [:create, :destroy]
     resources :comments, only: [:create]
   end
 
